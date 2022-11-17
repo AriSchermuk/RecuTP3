fc1f52ad9638969f6dc7f807da5e89a552613a2d
Ari Schermuk <arielschermuk@gmail.com>
2022-11-17 00:43:02 -0300

    show param information in detai fragment

--- app/src/main/java/com/example/recutp3/fragments/UserDetailFragment.kt
+++ app/src/main/java/com/example/recutp3/fragments/UserDetailFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.recutp3.R
 
 class UserDetailFragment : Fragment() {
     lateinit var view1: View
+    lateinit var lblDescription: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +18,15 @@ class UserDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         view1 = inflater.inflate(R.layout.fragment_user_detail, container, false)
+
+        lblDescription = view1.findViewById(R.id.lblUserDetailDescription)
+
         return view1
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val userId = UserDetailFragmentArgs.fromBundle(requireArguments()).userId
+        lblDescription.text = getString(R.string.userDetail_description, userId)
     }
 }
