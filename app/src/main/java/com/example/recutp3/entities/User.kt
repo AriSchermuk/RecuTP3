c0834afcd2c0170be778eb9071e6a6dbed273303
Ari Schermuk <arielschermuk@gmail.com>
2022-11-17 01:30:15 -0300

    make User Parcelable instead of Serializable

--- app/src/main/java/com/example/recutp3/entities/User.kt
+++ app/src/main/java/com/example/recutp3/entities/User.kt
@@ -1,5 +1,30 @@
 package com.example.recutp3.entities
 
-import java.io.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
-class User(val userId: String) : Serializable
+class User(val userId: String) : Parcelable {
+
+
+
+    constructor(parcel: Parcel) : this(parcel.readString().toString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(userId)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
